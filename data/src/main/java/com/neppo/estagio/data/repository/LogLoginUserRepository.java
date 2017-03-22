79ec9b9d7bf362c2b728660df33765f87360f505
Sergio Santiago <sergio.a.santiago@gmail.com>
2017-03-22 16:37:53 -0300

    Create a data example project

---
+++ data/src/main/java/com/neppo/estagio/data/repository/LogLoginUserRepository.java
@@ -0,0 +1,7 @@
+package com.neppo.estagio.data.repository;
+
+import com.neppo.estagio.data.model.LogLoginUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LogLoginUserRepository extends CrudRepository<LogLoginUser, Long> {
+}
