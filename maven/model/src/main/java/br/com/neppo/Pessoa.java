70d63e8e3e93deb8755dd98c750245434c1ede6e
Antonio Filho <antoniodsaf@gmail.com>
2017-03-23 09:53:44 -0300

    maven content

---
+++ maven/model/src/main/java/br/com/neppo/Pessoa.java
@@ -0,0 +1,25 @@
+package br.com.neppo;
+
+public class Pessoa {
+
+	private Integer idade;
+    private String nome;
+
+
+	public Integer getIdade(){
+		return idade;
+	}
+
+	public void setIdade(Integer idade){
+		this.idade = idade;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+}
+
