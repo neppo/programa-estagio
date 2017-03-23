ed436ff460c2507b3e0ca9383d56fca981f01c05
Gustavo Falleiros <gustavo.falleiros@neppo.com.br>
2017-03-23 17:06:25 -0300

    fix

---
+++ service-api/src/test/java/TestLogin.java
@@ -0,0 +1,62 @@
+import com.neppo.estagio.data.model.User;
+import com.neppo.estagio.service.data.UserService;
+import com.neppo.estagio.service.login.LoginHandler;
+import com.neppo.estagio.service.login.LoginService;
+import com.neppo.estagio.service.login.impl.ConsoleLogObserverImpl;
+import com.neppo.estagio.service.login.impl.DatabaseLogObserverImpl;
+import com.neppo.estagio.service.login.impl.LoginServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestLogin {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ConsoleLogObserverImpl consoleLogObserver;
+
+    @Mock
+    private DatabaseLogObserverImpl databaseLogObserver;
+
+    @Mock
+    private LoginHandler loginHandler;
+
+
+    @Before
+    public void init(){
+
+        when(userService.findByUsername(any())).thenReturn(new User("teste", "lalala", "Teste", "Testado"));
+    }
+
+
+    @Test
+    public void testSuccess(){
+        LoginService loginService = new LoginServiceImpl(userService, loginHandler, consoleLogObserver, databaseLogObserver);
+
+        boolean success = loginService.login("teste", "lalala");
+
+        assertTrue(success);
+
+    }
+
+    @Test
+    public void testFail(){
+        LoginService loginService = new LoginServiceImpl(userService, loginHandler, consoleLogObserver, databaseLogObserver);
+
+        boolean success = loginService.login("teste", "lalal");
+
+        assertFalse(success);
+
+    }
+}
