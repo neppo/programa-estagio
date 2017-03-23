841f0871285c9511d61c59ed5620f938e5a606f8
Sérgio Anchieta Santiago <sergio.a.santiago@gmail.com>
2017-03-23 10:23:29 -0300

    Update Pessoa.java

--- maven/model/src/main/java/br/com/neppo/Pessoa.java
+++ maven/model/src/main/java/br/com/neppo/Pessoa.java
@@ -2,24 +2,22 @@ package br.com.neppo;
 
 public class Pessoa {
 
-	private Integer idade;
+    private Integer idade;
     private String nome;
 
+    public Integer getIdade(){
+        return idade;
+    }
 
-	public Integer getIdade(){
-		return idade;
-	}
+    public void setIdade(Integer idade){
+        this.idade = idade;
+    }
 
-	public void setIdade(Integer idade){
-		this.idade = idade;
-	}
+    public String getNome(){
+        return nome;
+    }
 
-	public String getNome(){
-		return nome;
-	}
-
-	public void setNome(String nome){
-		this.nome = nome;
-	}
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
-
